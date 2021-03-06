41b52eb93967f980364b24ae5db93983da652a6b
IkumiKanata <i.kanata0531@gmail.com>
2021-03-07 07:48:33 +0900

    understand firebase where and limit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react"
+import React, {useState} from "react"
 import firebase from 'firebase';
 
 const  firebaseConfig = {
@@ -19,7 +19,7 @@ const  firebaseConfig = {
 
 
 function App() {
-  
+  const [users, setUsers] = useState([]);
   const hundleClickFetchButton = async () => { 
     const db = firebase.firestore();
     // document取得
@@ -27,16 +27,29 @@ function App() {
   //   console.log(doc.data());
   //   console.log("fetch button");
 
-    db.collection("users").get().then((snapshot) => {
+  //collection 取得
+    const snapshot = await db.collection("users")
+    .get();
+    const _users = [];
       snapshot.forEach(doc => {
-        console.log(doc.data())
+       _users.push({
+         userId: doc.id,
+         ...doc.data()
+       })
       })
-    })
+    setUsers(_users)
   }
+
+  const userListItems = users.map(user => {
+    return (
+      <li key={user.userId}>{user.name} : {user.age} : {user.location} </li>
+    )
+  })
   return (
     <div className="App">
      Hello2
      <button onClick={hundleClickFetchButton}>取得</button>
+     <ul>{userListItems}</ul>
     </div>
 
   );
